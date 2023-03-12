4a035b00353c04470d7faf28fbf6f350cc4c0057
Stefan Mesken <kappadistributive@gmail.com>
2023-03-12 17:48:03 +0100

    minor refactoring

--- forward_forward/main.py
+++ forward_forward/main.py
@@ -45,20 +45,24 @@ def overlay_label(images: Tensor, labels: Tensor) -> Tensor:
 
 
 class Layer(nn.Linear):
-    def __init__(self, in_features: int, out_features: int, bias: bool, lr: float, threshold: float, device: Optional[torch.device] = None,
-                 dtype=None) -> None:
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool,
+        lr: float,
+        threshold: float,
+        device: Optional[torch.device] = None,
+        dtype=None,
+    ) -> None:
         super().__init__(in_features=in_features, out_features=out_features, bias=bias, device=device, dtype=dtype)
-        self.layer_norm = nn.LayerNorm(normalized_shape=in_features, elementwise_affine=False, device=device, dtype=dtype)
         self.activation = nn.ReLU()
         self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         self.threshold = threshold
 
     def forward(self, x: Tensor) -> Tensor:
-        # x_direction = self.layer_norm(x)
-        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
-        return self.activation(
-            torch.mm(x_direction, self.weight.T) + self.bias.unsqueeze(0)
-        )
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-5)
+        return self.activation(torch.mm(x_direction, self.weight.T) + self.bias.unsqueeze(0))
 
     def train(self, x_positive: Tensor, x_negative: Tensor) -> (Tensor, Tensor, Tensor):
         bus_positive = self.forward(x_positive)
@@ -67,10 +71,10 @@ class Layer(nn.Linear):
         goodness_positive = bus_positive.pow(2).mean(1)
         goodness_negative = bus_negative.pow(2).mean(1)
 
-        loss = torch.log(1 + torch.exp(torch.cat([
-            -goodness_positive + self.threshold,
-            goodness_negative - self.threshold
-            ]))).mean()
+        # [Logistic Margin Loss](http://juliaml.github.io/LossFunctions.jl/stable/losses/margin/#LogitMarginLoss-1)
+        loss = torch.log(
+            1 + torch.exp(-torch.cat([goodness_positive - self.threshold, -goodness_negative + self.threshold]))
+        ).mean()
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -79,23 +83,33 @@ class Layer(nn.Linear):
         return bus_positive.detach(), bus_negative.detach(), loss
 
 
-
 class Net(nn.Module):
-    def __init__(self, dimensions: list[int], lr : float, threshold: float, device: Optional[torch.device] = None, dtype=None):
+    def __init__(
+        self, dimensions: list[int], lr: float, threshold: float, device: Optional[torch.device] = None, dtype=None
+    ):
         super().__init__()
         self.layers: list[Layer] = []
         for index in range(len(dimensions) - 1):
-            self.layers.append(Layer(dimensions[index], dimensions[index + 1], bias=True, lr=lr, threshold=threshold, device=device, dtype=dtype))
-
+            self.layers.append(
+                Layer(
+                    dimensions[index],
+                    dimensions[index + 1],
+                    bias=True,
+                    lr=lr,
+                    threshold=threshold,
+                    device=device,
+                    dtype=dtype,
+                )
+            )
 
     def train(self, x: Tensor, y: Tensor):
         x_positive = overlay_label(x, y)
 
         y_negative = torch.remainder(y + torch.randint(1, 10, y.shape), 10)
         x_negative = overlay_label(x, y_negative)
-        h_pos, h_neg = torch.flatten(x_positive, 1), torch.flatten(x_negative, 1)
+        hidden_positive, hidden_negative = torch.flatten(x_positive, 1), torch.flatten(x_negative, 1)
         for index, layer in enumerate(self.layers):
-            h_pos, h_neg, _ = layer.train(h_pos, h_neg)
+            hidden_positive, hidden_negative, _ = layer.train(hidden_positive, hidden_negative)
 
     def predict(self, x: Tensor) -> Tensor:
         goodness_per_label = []
@@ -110,10 +124,9 @@ class Net(nn.Module):
         return goodness_per_label.argmax(1)
 
 
-
 if __name__ == "__main__":
     train_loader, test_loader = MNIST_loaders(1000, 1000)
-    net = Net([28*28, 500, 500, 500, 500, 500, 500], lr=0.03, threshold=2.)
+    net = Net([28 * 28, 500, 500, 500, 500, 500, 500], lr=0.03, threshold=5.0)
     for epoch in range(500):
         print(f"Epoch #{epoch+1}")
         for x, y in train_loader:
